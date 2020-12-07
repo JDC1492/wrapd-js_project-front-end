444b08b22a72aa5c393b1045f7f0d7bb7709f3ea
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-12-07 17:28:08 -0500

    added first two event listeners

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(e){
-    
+ listSubListen()
 })
+// debugger
+
+function listSubListen(){
+    const subButton = document.getElementById("list_form")
+}
+
