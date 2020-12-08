3f77e1dd5d4ef7a28debd4994d23d50f2b335c5a
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-12-07 20:21:15 -0500

    function created to grab the inputed information

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ document.addEventListener("DOMContentLoaded", function(e){
 
 function listSubListen(){
     const subButton = document.getElementById("list_form")
-    subButton.addEventListener("submit", function(){
-        debugger
+    subButton.addEventListener("submit", function(event){
+        takeInput(event)
+        
     })
+    
+}
 
+function takeInput(event){
+    return {
+    receiver: event.target.querySelector('#receiver').value,
+    name: event.target.querySelector('#item-name').value,
+    price: event.target.querySelector('#item-price').value,
+    link: event.target.querySelector('#item-link').value,
+    check: event.target.querySelector('#obtained').value
+    }
 }
 
