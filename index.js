1b836a9060b3d496ca8709e4484da946181d15c0
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-12-09 20:15:36 -0500

    minor css changes

--- index.js
+++ index.js
@@ -1,72 +1,62 @@
 document.addEventListener("DOMContentLoaded", function(e){
     // loadLists()
     listSubListen()
-})
-        
-        const holidayList = document.getElementById("list-container")
-
-
-function listSubListen(){
-    //works
+})      
+    const holidayList = document.getElementById("list-container")
+function listSubListen(){//works
     const subButton = document.getElementById("list_form")
     subButton.addEventListener("submit", function(event){
         event.preventDefault()
     const userInput = takeInput(event)
     const listInfo = makeList(userInput)
         postList(listInfo)
-        clearForm()
-        
-    }
-    )
-    function takeInput(event){
-        //works
+        clearForm()    
+    })
+    function takeInput(event){//works
         return {
         listTitle: event.target.querySelector('#list-title').value,
         name: event.target.querySelector('#item-name').value,
         price: event.target.querySelector('#item-price').value
         }
     }
-
-    function makeList(list){
-        //works
+    function makeList(list){//works
         return `
         <div class="list">
-            <div>
+            <div class="strike">
                 <h2>A gift for: ${list.listTitle}</h2>
                 <h3>Item: ${list.name}</h3>
                 <h4>Price: ${list.price}</h4>
-                <br>
                 <label>Gift Obtained?</label><input id="obtained" type="checkbox" name="item_obtained">
+                <br>           
             </div>    
         </div>
         `
     }
-
-    function postList(list){
-        //works
+    function postList(list){//works
        const set = document.querySelector("#list-container").innerHTML += list
+       checkedBox()
     }
-
-    function clearForm(){
-        //works
+    function clearForm(){//works
         document.getElementById("list-title").value = ""
         document.getElementById("item-name").value = ""
         document.getElementById("item-price").value = ""
         }
-}        
+}       
 
 
 const checkedBox = function(){
       let check = document.getElementById("obtained")
       check.addEventListener("click", function(e){
-          
+          if(check && check.checked){
+            strikeIt()
+          }
       })
 }
 
+    const strikeIt = function(){
+         const done = document.querySelector('.strike')
+        
+        
+    }
 
        
-
-
-
-    
-   
